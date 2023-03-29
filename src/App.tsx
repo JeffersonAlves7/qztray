cccf3a05f1780407870ecab062b3404407e6f525
JeffersonAlves7 <ilj031420@hotmail.com>
2023-03-29 16:21:49 -0300

    feat: test pdf react

--- src/App.tsx
+++ src/App.tsx
@@ -6,31 +6,24 @@ const impressoraName = ""
 function App() {
   const [impressoras, setImpressoras] = useState<string[]>([])
 
-  function imprimir(){
-    qz.websocket.connect()
-      .then(() => {
-        return qz.printers.find()
-      })
-      .then((printers: string[]) => {
-        const config  = qz.configs.create(
-          impressoraName || printers.filter(printer => printer.toLowerCase().includes("zebra"))[0]
-        )
-        return qz.print(config, [{
+  async function imprimir() {
+    console.log('imprimir')
+    try {
+      const printer = await qz.printers.find("Microsoft XPS Document Writer")
+      var config = qz.configs.create(printer);       // Create a default config for the found printer
+      const data = [
+        {
           type: 'pixel',
-          format: ' html',
-          flavor: 'plain',
-          data: (document.querySelector("ul")?.innerHTML)
-        }])
-      })
-      .then(() => {
-        return qz.websocket.disconnect()
-      })
-      .then(() => {
-        //  process.exit(0)
-      })
-      .catch((err: any) => {
-        console.error(err ?? "Erro")
-      })
+          format: 'pdf',
+          flavor: 'file',
+          data: 'assets/14323982_1.pdf'
+        }
+      ]
+      return qz.print(config, data);
+    }
+    catch (e) {
+      console.log(e)
+    }
   }
 
 
