d6f52e8c18205d53acc2e07eebe1170b1b3dcda6
JeffersonAlves7 <ilj031420@hotmail.com>
2023-03-28 23:27:38 -0300

    feat: update qztray

--- scripts/script.js
+++ scripts/script.js
@@ -3,12 +3,15 @@ const printButton = document.getElementById('imprimir')
 try {
     qz.websocket.connect().then(() => {
         alert("connected")
+	findPrinters()
     })
 }
 catch (e) { }
 
 function displayMessage(message) {
-    document.querySelector("ul").innerHTML(message)
+	const ul = document.querySelector("ul")
+	console.log({ul, message})
+	ul.innerHTML = message
 }
 
 function findPrinters() {
@@ -27,12 +30,10 @@ function findPrinters() {
 printButton.onclick = async () => {
     console.log('imprimir')
     try{
-        const printer = await qz.printers.find("zebra")
+        const printer = await qz.printers.find("ZDesigner ZD220-203dpi ZPL")
         var config = qz.configs.create(printer);       // Create a default config for the found printer
         var data = ['^XA^FO50,50^ADN,36,20^FDRAW ZPL EXAMPLE^FS^XZ'];   // Raw ZPL
         return qz.print(config, data);
     }
     catch(e){ }
 }
-
-window.onload = () => {findPrinters()}
